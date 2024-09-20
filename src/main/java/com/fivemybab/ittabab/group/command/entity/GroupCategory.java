870738c30f76e9bc9e3eb72db08154e65ee8f19a
Pangtaek <zombie3632@gmail.com>
2024-09-20 15:06:18 +0900

    feat: 모임 조회, 상세 조회 구현

--- src/main/java/com/fivemybab/ittabab/group/command/entity/GroupCategory.java
+++ src/main/java/com/fivemybab/ittabab/group/command/entity/GroupCategory.java
@@ -1,13 +1,12 @@
 package com.fivemybab.ittabab.group.command.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "group_category_id ")
 public class GroupCategory {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int groupCategoryId;
     private String groupCategoryName;
 }
