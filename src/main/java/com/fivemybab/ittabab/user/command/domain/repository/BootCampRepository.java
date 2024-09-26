925d84fd97e4100353159793b74f092156cc53e0
seolbin01 <se01bin@suwon.ac.kr>
2024-09-26 15:08:45 +0900

    fix: mybatis 설정 수정

--- src/main/java/com/fivemybab/ittabab/user/command/domain/repository/BootCampRepository.java
+++ src/main/java/com/fivemybab/ittabab/user/command/domain/repository/BootCampRepository.java
@@ -4,6 +4,5 @@ import com.fivemybab.ittabab.bootcamp.command.entity.BootCamp;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BootCampRepository extends JpaRepository<BootCamp, Long> {
 }
