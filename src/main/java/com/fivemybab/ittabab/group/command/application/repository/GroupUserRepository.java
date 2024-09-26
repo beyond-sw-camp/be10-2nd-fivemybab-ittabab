78b6ce9bca7ac035706f771454a9a46743140371
seolbin01 <se01bin@suwon.ac.kr>
2024-09-26 15:28:11 +0900

    fix: import 수정

--- src/main/java/com/fivemybab/ittabab/group/command/application/repository/GroupUserRepository.java
+++ src/main/java/com/fivemybab/ittabab/group/command/application/repository/GroupUserRepository.java
@@ -1,6 +1,6 @@
 package com.fivemybab.ittabab.group.command.application.repository;
 
-import com.fivemybab.ittabab.group.command.domain.entity.GroupUser;
+import com.fivemybab.ittabab.group.command.domain.aggregate.GroupUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GroupUserRepository extends JpaRepository<GroupUser, Long> {
