99150fced0f4405b68d7a3f0c593495b3e1a5fff
enking <engur9902@gmail.com>
2024-09-29 15:29:40 +0900

    feature: board 에러 해결

--- src/main/java/com/fivemybab/ittabab/board/query/dto/BoardQueryDto.java
+++ src/main/java/com/fivemybab/ittabab/board/query/dto/BoardQueryDto.java
@@ -1,13 +1,10 @@
 package com.fivemybab.ittabab.board.query.dto;
 
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
-@Getter
-@Setter
-@ToString
+
+@Data
 public class BoardQueryDto {
     private Long postId;       // 게시물 ID
     private String title;      // 제목
@@ -16,5 +13,6 @@ public class BoardQueryDto {
     private int commentCount;   // 댓글 수
     private boolean isBlinded;  // 블라인드 여부
     private String createdAt;   // 생성일
-    private String memberName;  // 게시물 작성자 이름 추가
+
+
 }
