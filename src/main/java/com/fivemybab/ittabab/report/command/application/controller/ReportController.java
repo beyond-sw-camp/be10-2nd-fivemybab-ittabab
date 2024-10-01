80ece8e11a2e576e66ae95e59befa186b794df8d
minju <kminju0224@gmail.com>
2024-10-02 02:33:40 +0900

    feat: 신고 @AuthenticationPrincipal로 userI 가져옴

--- src/main/java/com/fivemybab/ittabab/report/command/application/controller/ReportController.java
+++ src/main/java/com/fivemybab/ittabab/report/command/application/controller/ReportController.java
@@ -6,11 +6,13 @@ import com.fivemybab.ittabab.report.command.application.dto.CreateReportRequest;
 import com.fivemybab.ittabab.report.command.application.dto.ResolveReportRequest;
 import com.fivemybab.ittabab.report.command.application.dto.ResolveReportResponse;
 import com.fivemybab.ittabab.report.command.application.service.ReportService;
+import com.fivemybab.ittabab.security.util.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -26,23 +28,17 @@ public class ReportController {
     // 신고 생성
     @Operation(summary = "신고 생성")
     @PostMapping
-    public ResponseEntity<CreateReportRequest> createReport(@RequestBody CreateReportRequest createReportRequest, @RequestParam Long userId) {
+    public ResponseEntity<CreateReportRequest> createReport(@RequestBody CreateReportRequest createReportRequest, @AuthenticationPrincipal CustomUserDetails loginUser) {
         createReportRequest.setCreateDate(LocalDateTime.now());
-        reportService.createReport(createReportRequest, userId);
+        reportService.createReport(createReportRequest, loginUser.getUserId());
         return new ResponseEntity<>(createReportRequest,HttpStatus.CREATED);
     }
 
-//    // 모든 신고 조회
-//    @GetMapping("/list")
-//    public ResponseEntity<List<ReportDto>> getAllReports() {
-//        List<ReportDto> reportList =reportService.getAllReports();
-//        return new ResponseEntity<>(reportList,HttpStatus.OK);
-//    }
 
     // 신고 처리 (관리자만 가능)
     @Operation(summary = "신고 처리 (관리자)")
     @PostMapping("/{reportId}")
     public ResponseEntity<ResolveReportResponse> resolveReport(@PathVariable Long reportId,@RequestBody ResolveReportRequest request) {
-        return ResponseEntity.ok(reportService.resolveReport(reportId, request.getUserId()));
+        return ResponseEntity.ok(reportService.resolveReport(reportId, request));
     }
 }
