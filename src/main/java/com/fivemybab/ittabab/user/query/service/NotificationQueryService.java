b23f7702b3d324e9de213457f9a08720c9cecbf7
seolbin01 <se01bin@suwon.ac.kr>
2024-10-02 02:32:12 +0900

    feat: notification 조회 테스트 추가

--- src/main/java/com/fivemybab/ittabab/user/query/service/NotificationQueryService.java
+++ src/main/java/com/fivemybab/ittabab/user/query/service/NotificationQueryService.java
@@ -4,6 +4,7 @@ import com.fivemybab.ittabab.user.query.dto.NotificationDto;
 import com.fivemybab.ittabab.user.query.mapper.NotificationMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class NotificationQueryService {
 
     private final NotificationMapper notificationMapper;
 
+    @Transactional(readOnly = true)
     public List<NotificationDto> findNotificationList(Long userId) {
         return notificationMapper.findNotificationList(userId);
     }
