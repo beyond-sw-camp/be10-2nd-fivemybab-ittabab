6e749ffe3493a984066f7a4439a04da9a6bb17f0
신민철 <dbeoalscjf98@naver.com>
2024-10-02 16:01:52 +0900

    특정 유저 주문 메뉴 전체 조회

--- src/main/java/com/fivemybab/ittabab/store/query/controller/StoreOrderMenuQueryController.java
+++ src/main/java/com/fivemybab/ittabab/store/query/controller/StoreOrderMenuQueryController.java
@@ -1,6 +1,8 @@
 package com.fivemybab.ittabab.store.query.controller;
 
 import com.fivemybab.ittabab.store.command.application.dto.StoreOrderMenuInfoDto;
+import com.fivemybab.ittabab.store.command.application.dto.StoreUserOrderMenuInfoDto;
+import com.fivemybab.ittabab.store.query.service.StoreMenuCategoryQueryService;
 import com.fivemybab.ittabab.store.query.service.StoreOrderMenuQueryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -31,12 +33,22 @@ public class StoreOrderMenuQueryController {
         return new ResponseEntity<>(storeOrderMenuList, HttpStatus.OK);
     }
 
+    /* 특정 유저 주문 메뉴 전체 조회 */
+    @Operation(summary = "특정 유저 주문 메뉴 전체 조회")
+    @GetMapping("/list/{userId}")
+    public ResponseEntity<List<StoreUserOrderMenuInfoDto>> storeUserOrderMenuList(@PathVariable Long userId) {
+
+        List<StoreUserOrderMenuInfoDto> storeUserOrderMenuList = storeOrderMenuQueryService.findStoreUserOrderMenuByUserId(userId);
+        return new ResponseEntity<>(storeUserOrderMenuList, HttpStatus.OK);
+
+    }
+
     /* 특정 리뷰 주문 메뉴 조회 */
     @Operation(summary = "주문메뉴 상세 조회")
     @GetMapping("/detail/{orderId}")
     public ResponseEntity<StoreOrderMenuInfoDto> StoreOrderMenuDetail(@PathVariable Long orderId) {
-        StoreOrderMenuInfoDto storeOrderMenu = storeOrderMenuQueryService.findStoreOrderMenuByOrderId(orderId);
 
+        StoreOrderMenuInfoDto storeOrderMenu = storeOrderMenuQueryService.findStoreOrderMenuByOrderId(orderId);
         return new ResponseEntity<>(storeOrderMenu, HttpStatus.OK);
     }
 
--- src/main/java/com/fivemybab/ittabab/store/query/mapper/StoreOrderMenuMapper.java
+++ src/main/java/com/fivemybab/ittabab/store/query/mapper/StoreOrderMenuMapper.java
@@ -1,6 +1,7 @@
 package com.fivemybab.ittabab.store.query.mapper;
 
 import com.fivemybab.ittabab.store.command.application.dto.StoreOrderMenuInfoDto;
+import com.fivemybab.ittabab.store.command.application.dto.StoreUserOrderMenuInfoDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -14,4 +15,6 @@ public interface StoreOrderMenuMapper {
     /* 특정 리뷰 주문 메뉴 조회 */
     StoreOrderMenuInfoDto findStoreOrderMenuByOrderId(Long id);
 
+    /* 특정 유저 주문 메뉴 전체 조회 */
+    List<StoreUserOrderMenuInfoDto> findStoreUserOrderMenuByUserId(Long id);
 }
